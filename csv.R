acd4503d1be60e6eb73fbc920b0368d75f255fe1
suresh577 <sureshchittiboina9757@gmail.com>
2024-03-03 14:50:06 +0530

    working on csv files

---
+++ csv.R
@@ -0,0 +1,32 @@
+> csv <- read.csv("C:/Users/sures/Downloads/Sample-Spreadsheet-100-rows.csv", header=FALSE)
+> View(csv)
+> head(csv)
+V1                                                 V2                 V3  V4      V5     V6
+1  1   Eldon Base for stackable storage shelf, platinum Muhammed MacIntyre   3 -213.25  38.94
+2  2 1.7 Cubic Foot Compact "Cube" Office Refrigerators       Barry French 293  457.81 208.16
+3  3   Cardinal Slant-D® Ring Binder, Heavy Gauge Vinyl       Barry French 293   46.71   8.69
+4  4                                               R380      Clay Rozendal 483 1198.97 195.99
+5  5                           Holmes HEPA Air Purifier     Carlos Soltero 515   30.94  21.78
+6  6  G.E. Longer-Life Indoor Recessed Floodlight Bulbs     Carlos Soltero 515    4.43   6.64
+V7      V8                             V9  V10
+1 35.00 Nunavut         Storage & Organization 0.80
+2 68.02 Nunavut                     Appliances 0.58
+3  2.99 Nunavut Binders and Binder Accessories 0.39
+4  3.99 Nunavut   Telephones and Communication 0.58
+5  5.94 Nunavut                     Appliances 0.50
+6  4.95 Nunavut             Office Furnishings 0.37
+> tail(csv)
+V1                                    V2              V3    V4     V5     V6    V7
+95   95    Bevis Boat-Shaped Conference Table   Doug Bickford 10499  31.21 262.11 62.74
+96   96 Linden® 12" Wall Clock With Oak Frame   Doug Bickford 10535 -44.14  33.98 19.99
+97   97                            Newell 326   Doug Bickford 10535  -0.79   1.76  0.70
+98   98          Prismacolor Color Pencil Set    Jamie Kunitz 10789  76.42  19.84  4.10
+99   99            Xerox Blank Computer Paper Anthony Johnson 10791  93.36  19.98  5.77
+100 100                       600 Series Flip    Ralph Knight 10945   4.22  95.99  8.99
+                       V8                           V9  V10
+95  Northwest Territories                       Tables 0.75
+96  Northwest Territories           Office Furnishings 0.55
+97  Northwest Territories          Pens & Art Supplies 0.56
+98  Northwest Territories          Pens & Art Supplies 0.44
+99  Northwest Territories                        Paper 0.38
+100 Northwest Territories Telephones and Communication 0.57
